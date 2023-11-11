8d4c9daccee603eb98a9d04e286da7ded28112f6
SimplyJustMy <simplyjustmy@gmail.com>
2023-11-11 17:13:51 -0500

    About me page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,15 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'just-my';
-  showPhoneNumber: boolean = false;
-
-  // Handle the click event on the "Call +1 845-596-0635" link
-  callPhoneNumber(): void {
-    // You can add code here to initiate the phone call using a telephony API or other method.
-    // Note: Initiating a phone call directly from a webpage might not work in all browsers.
-
-    // For demonstration purposes, you can log a message to the console.
-    console.log('Initiating a phone call...');
-  }
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AboutUsComponent } from './about-us/about-us.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent,
+    AboutUsComponent
   ],
   imports: [
     BrowserModule,
