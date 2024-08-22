92e6137ed6229fde252d87691c7323621d6d30c7
--global <--global>
2024-08-22 16:22:35 -0400

    video fix

--- src/app/about-us/about-us.component.ts
+++ src/app/about-us/about-us.component.ts
@@ -12,15 +12,11 @@ interface TeamMember {
   templateUrl: './about-us.component.html',
   styleUrls: ['./about-us.component.scss']
 })
-export class AboutUsComponent {
+export class AboutUsComponent  {
 
 
   ngAfterViewInit(): void {
-    const heroVideo = document.getElementById('heroVideo') as HTMLVideoElement;
-    if (heroVideo) {
-      heroVideo.muted = true;
-      heroVideo.setAttribute('muted', 'true'); // Ensures attribute is set
-    }
+    this.playHeroVideo();
   }
 
   teamRows: TeamMember[][] = [];
@@ -88,5 +84,21 @@ export class AboutUsComponent {
 
   hideDetails(member: any) {
     member.showDetails = false;
+  }
+
+  private playHeroVideo(): void {
+    const heroVideo = document.getElementById('heroVideo') as HTMLVideoElement;
+    if (heroVideo) {
+      heroVideo.muted = true;
+      heroVideo.setAttribute('muted', 'true');
+      heroVideo.autoplay = true;
+      heroVideo.setAttribute('autoplay', 'true');
+      heroVideo.loop = true;
+      heroVideo.setAttribute('loop', 'true');
+      heroVideo.play().catch(error => {
+        // Handle video play error if needed
+        console.error('Error attempting to play video:', error);
+      });
+    }
   }
 }
--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -9,4 +9,23 @@ import { FirebaseService } from '../firebase.service';
 })
 export class ContactComponent {
 
+  ngAfterViewInit(): void {
+    this.playHeroVideo();
+  }
+
+  private playHeroVideo(): void {
+    const heroVideo = document.getElementById('heroVideo') as HTMLVideoElement;
+    if (heroVideo) {
+      heroVideo.muted = true;
+      heroVideo.setAttribute('muted', 'true');
+      heroVideo.autoplay = true;
+      heroVideo.setAttribute('autoplay', 'true');
+      heroVideo.loop = true;
+      heroVideo.setAttribute('loop', 'true');
+      heroVideo.play().catch(error => {
+        // Handle video play error if needed
+        console.error('Error attempting to play video:', error);
+      });
+    }
+  }
 }
